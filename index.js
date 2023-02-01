e0899b3822bc7b5f8cd18479e7b418373f8cbaa5
Saki <sbasken@gmail.com>
2023-02-01 10:07:24 -0800

    add functions to calculate fares and assign drivers

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution in this file!
+
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+const returnFirstTwoDrivers = (arr) => {
+  return arr.slice(0, 2)
+}
+returnFirstTwoDrivers(drivers)
+
+const returnLastTwoDrivers = (arr) => {
+    return arr.slice(2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = (int) => {
+    return function (fare) {
+        return fare * int
+    }
+}
+const innerFunction = createFareMultiplier(5)
+console.log(innerFunction)
+
+
+const fareDoubler = createFareMultiplier(2)
+console.log(fareDoubler(2))
+
+const fareTripler = createFareMultiplier(3)
+console.log(fareTripler(2))
+
+const selectDifferentDrivers = (drivers, func) => {
+    let selectDifferentDrivers = [];
+    if (func === returnFirstTwoDrivers) {
+        selectDifferentDrivers = returnFirstTwoDrivers(drivers)
+    } else if (func === returnLastTwoDrivers) {
+        selectDifferentDrivers = returnLastTwoDrivers(drivers)
+    }
+    return selectDifferentDrivers
+}
+
+console.log(selectDifferentDrivers(drivers, returnFirstTwoDrivers))
